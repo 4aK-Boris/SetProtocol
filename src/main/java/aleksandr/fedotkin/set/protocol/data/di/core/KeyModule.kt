de1d6ecbfc25da24cb3183783c56fc27bb57f281
dmitriy <dmitriylosevxxx@gmail.com>
2022-12-31 01:46:18 +0300

    Удалены ненужные файлы

--- src/main/java/aleksandr/fedotkin/set/protocol/data/di/core/KeyModule.kt
+++ src/main/java/aleksandr/fedotkin/set/protocol/data/di/core/KeyModule.kt
@@ -1,13 +1,11 @@
-package aleksandr.fedotkin.set.protocol.data.di.crypto
+package aleksandr.fedotkin.set.protocol.data.di.core
 
+import aleksandr.fedotkin.set.protocol.core.ASYMMETRIC_KEY_LENGTH
 import aleksandr.fedotkin.set.protocol.core.CIPHER_ALGORITHM
 import aleksandr.fedotkin.set.protocol.core.RSA
 import aleksandr.fedotkin.set.protocol.core.SYMMETRIC_KEY_LENGTH
 import java.security.KeyFactory
-import java.security.KeyPair
 import java.security.KeyPairGenerator
-import java.security.PrivateKey
-import java.security.PublicKey
 import javax.crypto.KeyGenerator
 import org.koin.dsl.module
 
@@ -25,19 +23,7 @@ val keyModule = module {
 
     single<KeyPairGenerator> {
         KeyPairGenerator.getInstance(RSA).apply {
-            initialize(2048, get())
+            initialize(ASYMMETRIC_KEY_LENGTH, get())
         }
-    }
-
-    single<KeyPair> {
-        get<KeyPairGenerator>().genKeyPair()
-    }
-
-    single<PublicKey> {
-        get<KeyPair>().public
-    }
-
-    single<PrivateKey> {
-        get<KeyPair>().private
     }
 }
